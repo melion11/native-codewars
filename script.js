8e5690353d1239bf3c9aa96e1e61a9277ee28d22
melion11 <110605782+melion11@users.noreply.github.com>
2023-03-30 22:46:36 +0300

    +1 codewars kata

--- script.js
+++ script.js
@@ -124,3 +124,30 @@ result = 61000 */
 // }
 
 // console.log(isSquare(25));
+
+
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+// Note: a and b are not ordered!
+
+function getSum(a, b) {
+	let sum = 0;
+	if (a === b) {
+		return a || b;
+	}
+
+	if (a > b) {
+		for (let i = b; i <= a; i++) {
+			sum += i;
+		}
+		return sum;
+	} else if (b > a) {
+		for (let i = a; i <= b; i++) {
+			sum += i;
+		}
+		return sum;
+	}
+}
+
+let result = getSum(1, 4);
+
+console.log(result);
