5823d79824dc6bae6daef9fe0614402141e51b89
melion11 <110605782+melion11@users.noreply.github.com>
2023-03-31 11:13:54 +0300

    +1 codewars kata

--- script.js
+++ script.js
@@ -187,3 +187,30 @@ result = 61000 */
 // 		return length * width * height;
 // 	}
 // }
+
+
+// ___________________________________#2
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+// Mind the input validation.
+
+// function sumArray(array) {
+// 	if (!Array.isArray(array) || array.length <= 1) {
+// 		return 0;
+// 	}
+
+// 	let sum = 0;
+// 	let min = array[0];
+// 	let max = array[0];
+
+// 	for (let i = 0; i < array.length; i++) {
+// 		if (array[i] < min) {
+// 			min = array[i];
+// 		}
+// 		if (array[i] > max) {
+// 			max = array[i];
+// 		}
+// 		sum += array[i];
+// 	}
+// 	return sum - min - max;
+// }
